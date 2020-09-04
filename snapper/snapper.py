0a5db503fce1285ced0436905a413a00b71587e2
Bruce <bruce.bennett.code@gmail.com>
2020-09-04 10:42:38 -0400

    Added support for EC2 Volumes and Snapshots

--- snapper/snapper.py
+++ snapper/snapper.py
@@ -16,8 +16,70 @@ def filter_instances(project):
 
     return instances
 
-
+## Top level Click group
 @click.group()
+def cli():
+    """cloudsnap cli"""
+
+
+## Commands for Volumes
+@cli.group('volumes')
+def volumes():
+    """Commands for Volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+    
+    instances = filter_instances(project)
+
+    for i in instances:
+        
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                str(v.size) + " GiB",
+                v.state,
+                str(v.create_time),
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+    return
+
+## Commands for Snapshots
+@cli.group('snapshots')
+def snapshots():
+    """Commands for Snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(', '.join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c"),
+                    str(s.description)
+                )))
+
+    return
+
+
+## Commands for Instances
+@cli.group('instances')
 def instances():
     """Commands for Instances"""
 
@@ -43,6 +105,20 @@ def list_instances(project):
 
     return
 
+@instances.command('start')
+@click.option('--project', default=None,
+    help="only instances for project (tag Project:<name>)")
+def start_instances(project):
+    "Start EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+
+    return
+
 @instances.command('stop')
 @click.option('--project', default=None,
     help="only instances for project (tag Project:<name>)")
@@ -57,22 +133,41 @@ def stop_instances(project):
 
     return
 
-@instances.command('start')
+@instances.command('terminate')
 @click.option('--project', default=None,
     help="only instances for project (tag Project:<name>)")
-def stop_instances(project):
-    "Start EC2 instances"
+def terminate_instances(project):
+    "Terminate EC2 instances"
 
     instances = filter_instances(project)
 
     for i in instances:
-        print("Starting {0}...".format(i.id))
-        i.start()
+        print("Terminating {0}...".format(i.id))
+        i.terminate()
+
+    return
+
+@instances.command('snapshot')
+@click.option('--project', default=None,
+    help="only instances for project (tag Project:<name>)")
+def snapshot_instances(project):
+    "Snapshot EC2 Instance Volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()        #### NEEDS WORK... won't stop properly...
+        for v in i.volumes.all():
+            snapshot = v.create_snapshot(
+                Description='Snapshot created by CloudSnap'
+            )
+            print("Creating snapshot for volume {0} attached to {1}".format(v.id, i.id))
+            print("Snapshot ID: {0}".format(snapshot.id))
 
     return
 
 
-
+## Main
 if __name__ == '__main__':
-    instances()    
+    cli()    
     
